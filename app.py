d62b54f9bd49a4999af81dff40f733b92b3a551b
Andrew Wong <5258115+andrewKOwong@users.noreply.github.com>
2023-05-04 11:37:41 -0700

    Refactor: omnibus refactor.
    Includes:
    Function for converting codes to categorical.
    Refactoring chart prepartion.
    Code Flow.
    Comments.
    Groupby handling for no groupbys.

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ def select_region(keyed_survey_vars: dict):
 def select_groupby_var() -> str:
     return st.selectbox(
         label='Groupby:',
-        options=GROUPBY_VARS.keys(),
-        format_func=lambda k: GROUPBY_VARS[k])
+        options=[None] + list(GROUPBY_VARS.keys()),
+        format_func=lambda k: 'None' if k is None else GROUPBY_VARS[k])
 
 
 def remove_valid_skips(df: pd.DataFrame) -> pd.DataFrame:
@@ -80,6 +80,16 @@ def create_ordered_dtype(s: pd.Series) -> pd.CategoricalDtype:
     return pd.CategoricalDtype(
         categories=s.sort_values().unique(),
         ordered=True)
+
+
+def order_and_convert_code(
+        s: pd.Series,
+        keyed_survey_vars: dict) -> pd.Series:
+    """Converts a series of codes to text labels, as ordered categorical."""
+    return (s
+            .astype(create_ordered_dtype(s))
+            .cat.rename_categories(
+                svu.SurveyVar(keyed_survey_vars[s.name]).get_answer))
 
 
 @st.cache_data
@@ -113,41 +123,52 @@ def main(debug=False, log_file_path: str | None = None):
     plot_weighted = st.checkbox('Plot weighted frequency', value=True)
     remove_valid_skips = st.checkbox('Remove valid skips', value=True)
 
+    # BEGIN: DATA TRANSFORMATIONS
     # Region filtering
     if region is not None:
         df = tfm.filter_by_region(df, region)
     # Groupby filtering
-    df = df[[selected_var, groupby_var, WEIGHT_KEY]]
+    if groupby_var is None:
+        df = df[[selected_var, WEIGHT_KEY]]
+    else:
+        df = df[[selected_var, groupby_var, WEIGHT_KEY]]
 
     # Change ints to ordered categorical to preserve order,
     # then convert to text labels.
     # Mapping automatically converts categorical info.
-    # REFACTOR: using .cat.rename_categories() is more expressive.
     df = df.assign(**{
         selected_var: lambda d: (
-            d[selected_var].astype(create_ordered_dtype(d[selected_var]))
-            .map(lambda e: svu.SurveyVar(svs[selected_var]).get_answer(e))
-        ),
-        groupby_var: lambda d: (
-            d[groupby_var].astype(create_ordered_dtype(d[groupby_var]))
-            .map(svu.SurveyVar(svs[groupby_var]).get_answer)
-        )
-    })
+            order_and_convert_code(d[selected_var], svs))})
+    if groupby_var is not None:
+        df = df.assign(**{
+            groupby_var: lambda d: (
+                order_and_convert_code(d[groupby_var], svs))})
 
+    # Remove valid skips
     if remove_valid_skips:
         df = df.query(f"{selected_var} != '{VALID_SKIP}'")
+    # END: DATA TRANSFORMATIONS
 
+    # BEGIN: CHART PREPARATION
     # Hack to wrap long labels, for splitting in altair.
+    # `wrap` breaks long str into a list of str, then stitch them back together
+    # with LABEL_SPLIT delimiter. Altair then uses this delimiter to split
+    # the str again using Vega expressions.
     df = df.assign(**{
         selected_var: lambda d: d[selected_var].cat.rename_categories(
             lambda e: LABEL_SPLIT.join(wrap(e, 20)))
     })
 
+    # Assemble chart arguments
+    chart_args = {}
+    # Y is dependent on whether weights are used
     if not plot_weighted:
         y = alt.Y(f"count({selected_var}):Q", title='Count')
     else:
         y = alt.Y(f'sum({WEIGHT_KEY}):Q', title='Weighted Count')
-
+    chart_args['y'] = y
+    # X is bar chart labels. Sorting of the labels is by the original order
+    # in the codebook.
     x = alt.X(f"{selected_var}",
               type='ordinal',
               title=f"{selected_var} - {svs[selected_var][SVK.CONCEPT]}",
@@ -155,32 +176,42 @@ def main(debug=False, log_file_path: str | None = None):
               axis=alt.Axis(labelLimit=1000,
                             labelAngle=-45,
                             labelExpr=f"split(datum.label, '{LABEL_SPLIT}')"))
-
-    groupby_order = list(df[groupby_var].cat.categories)
-
-    chart = alt.Chart(df).mark_bar()
-    chart = chart.encode(
-        x=x,
-        y=y,
-        color=alt.Color(
+    chart_args['x'] = x
+    # Use color for groupbys
+    if groupby_var is not None:
+        groupby_order = list(df[groupby_var].cat.categories)
+        # Color is also ordered according to the codebook.
+        # For info about how to order both the legend and the order on the
+        # bar chart stacks, see:
+        # https://github.com/altair-viz/altair/issues/245#issuecomment-748443434
+        # The above is actually the official recommended solution according to
+        # the docs (search "follow the approach in this issue comment"):
+        # https://altair-viz.github.io/user_guide/encodings/channels.html#order
+        color = alt.Color(
             f"{groupby_var}:O",
             title=GROUPBY_VARS[groupby_var],
-            sort=alt.Sort(groupby_order)),
-        order=alt.Order(f'color_{groupby_var}_sort_index:Q', sort='ascending')
-    )
+            sort=alt.Sort(groupby_order))
+        order = alt.Order(
+            f'color_{groupby_var}_sort_index:Q', sort='ascending')
+        chart_args['color'] = color
+        chart_args['order'] = order
 
+    # Make the chart object
+    chart = alt.Chart(df).mark_bar().encode(**chart_args)
+    # END: CHART PREPARATION
+
+    # Plot the chart
     st.altair_chart(chart, use_container_width=True)
-
+    # Hint for the user on how to save the chart
     st.markdown(SAVE_HINT)
 
+    # Logging for debugging
     if debug:
         import logging
         logging.basicConfig(filename=log_file_path, level=logging.DEBUG)
         alt.data_transformers.disable_max_rows()
         logging.debug(chart.to_json())
 
-    # TODO Color order is wrong?!?!
-    # TODO Add no groupby option
     # TODO dealing with when region is in or not
     # TODO tool tips and such
     # TODO Handle PROBCNTP and VERDATE
