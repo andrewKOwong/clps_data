69759a220ce836f7c3435993c870f80ff321d745
Andrew Wong <5258115+andrewKOwong@users.noreply.github.com>
2023-05-04 16:20:07 -0700

    Remove: unused SurveyVar class

--- clps/survey_vars_utils.py
+++ clps/survey_vars_utils.py
@@ -231,31 +231,6 @@ class SurveyVars:
         return self._survey_vars[self.REGION_KEY]
 
 
-class SurveyVar:
-    """A class to represent a survey variable for convenient access.
-    """
-    # TODO deal with verdate and probcntp issue
-
-    def _generate_answer_lookup(self) -> dict:
-        """Generate a lookup table for the codes of each variable."""
-        return {c: a for c, a in zip(self.codes, self.ans_cats)}
-
-    def __init__(self, survey_var: dict):
-        """
-        Args:
-            survey_var: A survey variable dictionary, i.e. individual list
-                items.
-        """
-        raw = self.raw = survey_var.copy()
-        self.codes = [int(c) for c in raw[SVK.CODE]]
-        self.ans_cats = raw[SVK.ANSWER_CATEGORIES]
-        self._code_lookup = self._generate_answer_lookup()
-
-    def get_answer(self, code: int) -> str:
-        """Get a corresponding answer for an answer code."""
-        return self._code_lookup[code]
-
-
 def load_survey_vars(fp: str | Path) -> list:
     """Load the survey variables from the JSON file as a list."""
     if isinstance(fp, str):
@@ -271,8 +246,3 @@ def load_keyed_survey_vars(fp: str | Path) -> dict:
     with fp.open() as f:
         data = json.load(f)
     return {e[SVK.VAR_NAME]: e for e in data}
-
-
-def extract_survey_var(survey_vars: dict, key: str) -> dict:
-    """Extract the survey variable from the survey vars dictionary."""
-    return survey_vars[key]
