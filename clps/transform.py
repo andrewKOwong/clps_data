99d584302ac7e021cf3d065683d4a272bf85ab70
Andrew Wong <5258115+andrewKOwong@users.noreply.github.com>
2023-05-25 14:38:13 -0700

    Fix: Comments, docstrings, etc. for transform submodule

--- clps/transform.py
+++ clps/transform.py
@@ -5,10 +5,12 @@ from clps.survey_vars_utils import SurveyVars
 
 
 def filter_by_region(df: pd.DataFrame, region: int | None) -> pd.DataFrame:
-    """Filter a dataframe by a region code.
+    """Filter CLPS raw dataframe by a region code.
+
     Args:
         df: Dataframe to filter. Region column must be ints.
         region: Region code to filter by. If `None`, no filtering is done.
+
     Returns:
         Filtered dataframe.
     """
@@ -23,10 +25,15 @@ def filter_by_selected_and_groupby(
         groupby_var: str | None) -> pd.DataFrame:
     """Filter for the selected variable of interest, the groupby variable, and
     the respondent weights.
+
     Args:
         df: Dataframe with survey variables.
         selected_var: Name of the survey variable to filter for.
         groupby_var: Name of the groupby variable to filter for.
+
+    Returns:
+        Dataframe with selected and/or groupby variable columns, and the weight
+        column.
     """
     if groupby_var is None:
         df = df[[selected_var, WEIGHT_KEY]]
@@ -37,11 +44,13 @@ def filter_by_selected_and_groupby(
 
 def create_ordered_dtype(s: pd.Series) -> pd.CategoricalDtype:
     """From an integer-containing column, create an ordered categorical dtype.
+
     Args:
         s: Series corresponding to a survey variable, with integer codes.
+
     Returns:
         Ordered categorical dtype, with categories in ascending integer order.
-        """
+    """
     return pd.CategoricalDtype(
         categories=s.sort_values().unique(),
         ordered=True)
@@ -51,10 +60,13 @@ def order_and_convert_code(
         s: pd.Series,
         survey_vars: SurveyVars) -> pd.Series:
     """Converts a series of codes to text labels, as ordered categorical.
+
     Used as a helper func for `convert_to_categorical`.
+
     Args:
         s: Series corresponding to a survey variable, with integer codes.
         survey_vars: SurveyVars object, with survey variable metadata.
+
     Returns:
         Series with text labels as an ordered categorical. Order is determined
         by the order of the integers, which corresponds to the order found
@@ -62,7 +74,7 @@ def order_and_convert_code(
     """
     # Change ints to ordered categorical to preserve order,
     # then convert to text labels.
-    # Mapping automatically converts categorical info.
+    # Renaming categories automatically converts category names.
     return (s
             .astype(create_ordered_dtype(s))
             .cat.rename_categories(
@@ -73,14 +85,16 @@ def convert_to_categorical(
         df: pd.DataFrame,
         svs: SurveyVars,
         selected_var: str,
-        groupby_var: str | None):
+        groupby_var: str | None) -> pd.DataFrame:
     """Converted survey variable columns to ordered categorical dtype.
+
     Args:
         df: Dataframe with survey variable columns. These columns are still in
             integer code form.
         svs: SurveyVars object, with survey variable metadata.
         selected_var: Name of the survey variable column to convert.
         groupby_var: Name of the groupby column to convert, if any.
+
     Returns:
         Dataframe with survey variable columns converted to ordered categorical
         dtype, as text labels. Order is determined by the integer order, which
@@ -101,13 +115,17 @@ def filter_valid_skips(
         selected_var: str,
         remove_valid_skips: bool) -> pd.DataFrame:
     """Filter out valid skips from the data.
+
     If remove_valid_skips is `False` or `None`, this filter does nothing.
+
     Args:
         df: Dataframe with survey variable columns, converted to str ordered
         categorical dtype.
         selected_var: Name of the survey variable column of interest.
         remove_valid_skips: Whether to remove valid skips from the data.
+
     Returns:
+        Dataframe with valid skips removed from the selected variable column.
     """
     if remove_valid_skips:
         df = df.query(f"{selected_var} != '{VALID_SKIP}'")
@@ -124,7 +142,16 @@ def groupby_and_aggregate(
         groupby_var: str | None,
         weighted: bool,
         ) -> pd.DataFrame:
-    """Groupby and aggregate the dataframe."""
+    """Groupby and aggregate the dataframe.
+
+    Args:
+        df: Dataframe with survey variable columns.
+        selected_var: Name of the survey variable column of interest.
+        groupby_var: Name of the groupby column, if any.
+        weighted: Calculate the weight sums. Otherwise, counts the rows,
+            representing the number of respondents directly.
+
+    """
     # Assemble grouping variables
     groupby_list = [selected_var]
     if groupby_var is not None:
@@ -157,15 +184,29 @@ def transform_data(
         groupby_var: str | None,
         remove_valid_skips: bool | None,
         weighted: bool) -> pd.DataFrame:
-    # BEGIN: DATA TRANSFORMATIONS
-    # Filter region rows
+    """Transform CLPS data into a format suitable for plotting.
+
+    This is the main data transformation pipeline to filter, groupby, and
+    aggregate the raw CLPS data into a dataframe for plotting.
+
+    Args:
+        df: Dataframe with CLPS data.
+        survey_vars: SurveyVars object, with survey variable metadata.
+        region: Region code to filter the data by.
+        selected_var: Name of the survey variable column of interest.
+        groupby_var: Name of the groupby column, if any.
+        remove_valid_skips: Whether to remove valid skips from the data.
+        weighted: Calculate the weight sums. Otherwise, counts the rows,
+            representing the number of respondents directly.
+    """
+    # Filter region rows.
     df = filter_by_region(df, region)
-    # Filter survey var columns
+    # Filter survey var columns.
     df = filter_by_selected_and_groupby(df, selected_var, groupby_var)
-
+    # Replace integer codes with text labels, as ordered categorical dtype.
     df = convert_to_categorical(df, survey_vars, selected_var, groupby_var)
-
+    # Filter out valid skips
     df = filter_valid_skips(df, selected_var, remove_valid_skips)
+    # Groupby and aggregate
     df = groupby_and_aggregate(df, selected_var, groupby_var, weighted)
-    # END: DATA TRANSFORMATIONS
     return df
